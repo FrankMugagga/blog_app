d64833a9f94f2492429132941fbe25247a6c3ccb
FrankMugagga <frank.mugagga@gmail.com>
2023-07-08 21:04:15 +0300

    Update with additional info

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,7 +9,37 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
+    @comments = @post.comments
+    @comment = Comment.new
 
     render :show
+  end
+
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    # title = post_params[:title]
+    # text = post_params[:text]
+    # posts = Post.new(author_id: current_user.id, title: title, text: text, comments_counter: 0, likes_counter: 0 )
+    @post = current_user.posts.build(post_params)
+    @post.comments_counter = 0
+    @post.likes_counter = 0
+
+    if @post.save
+      # redirect_to user_posts_path(current_user, @post), notice: 'Post created successfully'
+      redirect_to user_posts_path, notice: 'Post saved successfully'
+    else
+      flash.now[:error] = @post.errors.full_messages.to_sentence
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
   has_many :posts, foreign_key: :author_id
-  has_many :likes, foreign_key: :author_id
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
   has_many :comments, foreign_key: :author_id
 
   validates :Name, presence: true
