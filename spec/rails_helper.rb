efb88a40bb168bd54315ecb63e22f048f782378f
FrankMugagga <frank.mugagga@gmail.com>
2023-07-11 09:08:00 +0300

    Add Capybara

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,4 +60,11 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  # capybara.default_driver= :selenium_chrome
+end
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :selenium_chrome_headless
+  end
 end
