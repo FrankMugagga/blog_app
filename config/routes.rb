b95486f98a72ee0f4d9422b6a914c31b2bff0ab9
FrankMugagga <frank.mugagga@gmail.com>
2023-07-12 22:36:44 +0300

    Add index route as user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    root to: 'devise/sessions#new'
+  end
+  
   get 'post/index'
   get 'post/show'
   get 'user/index'
