8d5f00b2da86001d9f89a4a57acff4db7eb77845
FrankMugagga <frank.mugagga@gmail.com>
2023-07-12 22:32:58 +0300

    Set environement for the devise operation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  host = localhost
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
