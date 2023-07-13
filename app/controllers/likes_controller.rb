8856d7be7994a58ae1b5ae99461c467c703cbac0
FrankMugagga <frank.mugagga@gmail.com>
2023-07-13 21:59:08 +0300

    Refactor for likes iteration

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,10 +1,12 @@
 class LikesController < ApplicationController
+
   def create
     post = Post.find(params[:post_id])
     @like = post.likes.new(author: current_user)
+
     if @like.save
-      post.increment!(:likes_counter)
-      redirect_to likes_path(current_user, post)
+      #post.increment!(:likes_counter)
+      redirect_to user_post_path(post.author, post)
     else
       render :new, status: :unprocessable_entity
     end
