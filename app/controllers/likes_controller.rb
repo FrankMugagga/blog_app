819bbebd862b10f56d68e258254c1eb23c3728a6
FrankMugagga <frank.mugagga@gmail.com>
2023-07-13 22:06:56 +0300

    Correct linter errors

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,10 @@
 class LikesController < ApplicationController
-
   def create
     post = Post.find(params[:post_id])
     @like = post.likes.new(author: current_user)
 
     if @like.save
-      #post.increment!(:likes_counter)
+      # post.increment!(:likes_counter)
       redirect_to user_post_path(post.author, post)
     else
       render :new, status: :unprocessable_entity
