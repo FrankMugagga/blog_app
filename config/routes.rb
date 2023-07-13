9be8a46967e4305d469ab9d82510e584b3957b1e
FrankMugagga <frank.mugagga@gmail.com>
2023-07-13 14:49:22 +0300

    Set the right routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  devise_scope :user do
-    root to: 'devise/sessions#new'
-  end
-  
+  root 'users#index'
   get 'post/index'
   get 'post/show'
   get 'user/index'
