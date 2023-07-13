dc366fd6bbdce294a47af9e5444c678bdca74668
FrankMugagga <frank.mugagga@gmail.com>
2023-07-13 16:19:39 +0300

    Correct the the likes routes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,27 +1,12 @@
 class LikesController < ApplicationController
-=begin
   def create
-    @post = Post.find(params[:post_id])
-    @like = @post.likes.find_or_initialize_by(author_id: current_user.id)
-
-    if @like.persisted?
-      # Like already exists, update it
-      @like.update(author_id: current_user.id)
-    elsif @like.save
-      # New like, save it
-      @post.increment!(:likes_counter)
+    post = Post.find(params[:post_id])
+    @like = post.likes.new(author: current_user)
+    if @like.save
+      post.increment!(:likes_counter)
+      redirect_to likes_path(current_user, post)
+    else
+      render :new, status: :unprocessable_entity
     end
-
-    redirect_to user_post_path(user_id: @user.id, id: @post.id), notice: 'Post successfully liked'
   end
-=end
-def create
-  post = Post.find(params[:id])
-  @like = post.likes.new(author: current_user)
-  if @like.save
-    redirect_to likes_path(current_user, post)
-  else
-    render :new, status: :unprocessable_entity
-  end
-end
 end
