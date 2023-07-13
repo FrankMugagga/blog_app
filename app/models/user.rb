da6c6e99db528f9f697a662d8d412831eab69b38
FrankMugagga <frank.mugagga@gmail.com>
2023-07-13 15:00:43 +0300

    Add user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id
 
   validates :Name, presence: true
-  validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  #validates :posts_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
