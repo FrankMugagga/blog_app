ece5cd501a2a208a0a0ec92a438d825d0a157c45
Rana-Mudassir <mduasire38@gmail.com>
2023-07-15 00:53:36 +0500

    Update ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,14 @@ class Ability
   def initialize(user)
     can :read, :all
     return unless user.present?
-
+    can :read, :all
+    return unless user.present?
+    can :manage, User, id: user.id
+    can :manage, Post, author_id: user.id
+    can :manage, Comment, author_id: user.id
+    can :create, Like
+    return unless user.admin?
+    can :manage, :all
     can :manage, User, id: user.id
     can :manage, Post, author_id: user.id
     can :manage, Comment, author_id: user.id
