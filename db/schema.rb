51eeccadbc93d39e9a139e2d322b81aaf63cb0c5
FrankMugagga <frank.mugagga@gmail.com>
2023-07-14 22:13:46 +0300

    Create a role column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_13_205954) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_14_145558) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_13_205954) do
     t.string "confirmation_token"
     t.string "unconfirmed_email"
     t.datetime "confirmation_sent_at"
+    t.string "role"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token"
     t.index ["confirmed_at"], name: "index_users_on_confirmed_at", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true, where: "((email IS NOT NULL) AND ((email)::text <> ''::text))"
