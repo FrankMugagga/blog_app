a330fb21c7bdc3dc7d8230a2652619658caef90e
FrankMugagga <frank.mugagga@gmail.com>
2023-07-15 16:41:51 +0300

    Remove errors

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -20,13 +20,13 @@ class Api::V1::CommentsController < ApplicationController
     comment.author_id = current_user.id
 
     if comment.save
-        render json: { success: true, message: 'Comment successfully created', comment: comment }, status: :created
+      render json: { success: true, message: 'Comment successfully created', comment: comment }, status: :created
     else
-        render json: { errors: comment.errors.full_message }, status: :unprocessable_entity
+      render json: { errors: comment.errors.full_message }, status: :unprocessable_entity
     end
-    rescue ActiveRecord::RecordNotFound
-        render json: { error: 'User or post not found' }, status: :not_found
-    end
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'User or post not found' }, status: :not_found
+  end
 
   private
 
