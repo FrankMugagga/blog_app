969b7e8b1e86c2d63234e6307e28582b10f2b935
Rana-Mudassir <mduasire38@gmail.com>
2023-07-15 18:36:49 +0500

    Update create method

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -19,6 +19,15 @@ class Api::V1::CommentsController < ApplicationController
     comment = post.comments.build(comment_params)
     comment.author_id = current_user.id
 
+    if comment.save
+        render json: { success: true, message: 'Comment successfully created', comment: comment }, status: :created
+    else
+        render json: { errors: comment.errors.full_message }, status: :unprocessable_entity
+    end
+    rescue ActiveRecord::RecordNotFound
+        render json: { error: 'User or post not found' }, status: :not_found
+    end
+
   private
 
   def comment_params
