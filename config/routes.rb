28069b5b747f99fc9fe79189e80263902cb98314
FrankMugagga <frank.mugagga@gmail.com>
2023-07-15 16:13:52 +0300

    Establish api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [] do
+      resources :posts, only: [:index] do
+        resources :comments, only: [:index, :create]
+      end
+    end
+    end
+  end
+
   devise_for :users
 
   root 'users#index'
